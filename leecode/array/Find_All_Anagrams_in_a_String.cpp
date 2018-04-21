1d907c3c8cdc6ffe1122630243ef7ad08ada3d15
cumtmiao <861106258@qq.com>
2018-04-21 22:39:31 +0800

    2018-4-21

--- leecode/array/Find_All_Anagrams_in_a_String.cpp
+++ leecode/array/Find_All_Anagrams_in_a_String.cpp
@@ -70,31 +70,31 @@ vector<int> findAnagrams(string s, string p)
 /*
 滑动窗口的办法。
 */
-// vector<int> findAnagrams(string s, string p) {
-//     int ns=s.size();
-//     int np=p.size();
-//     vector<int> res;
-//     if(np==0||ns==0||ns<np)
-//     return res;
-//     int count=np;//count表示还有多少个字符需要匹配  
-//     vector<int> m(256,0);//定义一个哈希表，存放p中的字符与数量的映射
-//     for(int i=0;i<np;i++)//将串p映射到hash表。
-//     {
-//         m[p[i]]++;
-//     }
-//     int left=0,right=0;//定义一个滑动窗口left,right分别表示滑动窗口的左边界和右边界
-//     //当右边界访问到字符串s的一个字符时，它的哈希值减一，当左边界访问到一个字符时，哈希值加1.
-//     //如果右边界访问的当前字符串在原始p串中出现过，则count--;
-//     //如果count变为0，则表示当前滑动窗口内的字符串与p完全匹配。此时将left放入结果集
-//     //如果right-left==np并且当前滑动窗口的左边界指向的字符在p中出现过，则count++
-//     while(right<ns)
-//     {
-//         if(m[s[right++]]-->=1)count--;
-//         if(count==0)res.push_back(left);
-//         if(right-left==np&&m[s[left++]]++>=0)count++;
-//     }
-//     return res;
-// }
+vector<int> findAnagrams(string s, string p) {
+    int ns=s.size();
+    int np=p.size();
+    vector<int> res;
+    if(np==0||ns==0||ns<np)
+    return res;
+    int count=np;//count表示还有多少个字符需要匹配  
+    vector<int> m(256,0);//定义一个哈希表，存放p中的字符与数量的映射
+    for(int i=0;i<np;i++)//将串p映射到hash表。
+    {
+        m[p[i]]++;
+    }
+    int left=0,right=0;//定义一个滑动窗口left,right分别表示滑动窗口的左边界和右边界
+    //当右边界访问到字符串s的一个字符时，它的哈希值减一，当左边界访问到一个字符时，哈希值加1.
+    //如果右边界访问的当前字符串在原始p串中出现过，则count--;
+    //如果count变为0，则表示当前滑动窗口内的字符串与p完全匹配。此时将left放入结果集
+    //如果right-left==np并且当前滑动窗口的左边界指向的字符在p中出现过，则count++
+    while(right<ns)
+    {
+        if(m[s[right++]]-->=1)count--;
+        if(count==0)res.push_back(left);
+        if(right-left==np&&m[s[left++]]++>=0)count++;
+    }
+    return res;
+}
 int  main()
 {
     string s1;
