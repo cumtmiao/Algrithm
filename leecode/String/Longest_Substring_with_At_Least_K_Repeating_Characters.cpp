a7f9e2afcea34f21e54e9303390dd0d6c1c2f58c
cumtmiao <861106258@qq.com>
2018-05-22 20:03:55 +0800

    2018-5-22

--- leecode/String/Longest_Substring_with_At_Least_K_Repeating_Characters.cpp
+++ leecode/String/Longest_Substring_with_At_Least_K_Repeating_Characters.cpp
@@ -24,62 +24,62 @@ The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated
 #include<bits/stdc++.h>
 using namespace std;
 //暴力解法.时间超时。O(n^3)
-// int longestSubstring(string s, int k) {
-//     int n=s.size();
-//     int res=0;
-//     for(int i=0;i<n;i++)
-//     {
-//         for(int j=i;j<n;j++)
-//         {
-//             vector<int> hash(26,0);
-//             for(int l=i;l<=j;l++)
-//             {
-//                 hash[s[l]-'a']++;
-//             }
-//             int l;
-//             for(l=i;l<=j;l++)
-//             {
-//                 if(hash[s[l]-'a']<k)
-//                 break;
-//             }
-//             if(l>j)
-//             res=max(res,j-i+1);
-//         }
-//     }
-//     return res;
-// }
+int longestSubstring(string s, int k) {
+    int n=s.size();
+    int res=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            vector<int> hash(26,0);
+            for(int l=i;l<=j;l++)
+            {
+                hash[s[l]-'a']++;
+            }
+            int l;
+            for(l=i;l<=j;l++)
+            {
+                if(hash[s[l]-'a']<k)
+                break;
+            }
+            if(l>j)
+            res=max(res,j-i+1);
+        }
+    }
+    return res;
+}
 /*
 解法二：一种巧妙的解法。利用了位运算。
 我们遍历字符串，对于每一个字符，我们都将其视为起点，然后遍历到末尾，我们增加哈希表中字母的出现次数，如果其小于k，
 我们将mask的对应位改为1，如果大于等于k，将mask对应位改为0。然后看mask是否为0，是的话就更新res结果，
 然后把当前满足要求的子字符串的起始位置j保存到max_idx中，等内层循环结束后，将外层循环变量i赋值为max_idx+1，继续循环直至结束.
 */
-// int longestSubstring(string s, int k) {
-//     int n=s.size();
-//     int res=0;
-//     int max_index;
-//     for(int i=0;i<=n-k;)
-//     {
-//         max_index=i;
-//         int mask=0;
-//         int hash[26]={0};
-//         for(int j=i;j<n;j++)
-//         {
-//             hash[s[j]-'a']++;
-//             if(hash[s[j]-'a']<k)
-//             mask|=(1<<(s[j]-'a'));
-//             else
-//             mask&=(~(1<<(s[j]-'a')));
-//             if(mask==0)
-//             {
-//                 res=max(res,j-i+1);
-//                 max_index=j;
-//             }
-//         }
-//         i=max_index+1;
-//     }
-//     return res;
-// }
+int longestSubstring(string s, int k) {
+    int n=s.size();
+    int res=0;
+    int max_index;
+    for(int i=0;i<=n-k;)
+    {
+        max_index=i;
+        int mask=0;
+        int hash[26]={0};
+        for(int j=i;j<n;j++)
+        {
+            hash[s[j]-'a']++;
+            if(hash[s[j]-'a']<k)
+            mask|=(1<<(s[j]-'a'));
+            else
+            mask&=(~(1<<(s[j]-'a')));
+            if(mask==0)
+            {
+                res=max(res,j-i+1);
+                max_index=j;
+            }
+        }
+        i=max_index+1;
+    }
+    return res;
+}
 /*
 思路三：
 利用分治法。
